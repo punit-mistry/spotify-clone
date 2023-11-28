28fea336da5f0c413440615ddfb1826fc060d538
Punit <Punitmistr@gmail.com>
2023-11-28 20:47:42 +0530

    half setup SideBar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,50 @@
+'use client'
+
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+import {HiHome} from 'react-icons/hi';
+import {BiSearch} from 'react-icons/bi'
+import Box from "./Box";
+import SidebarItem from "./SidebarItem";
 interface SidebarProps {
-    Children :React.ReactNode
+    children :React.ReactNode;
 }
 
-const Sidebar:ReactFC<SidebarProps> =({Children})=>{
+const Sidebar:React.FC<SidebarProps> =({children})=>{
+    const pathname = usePathname();
+
+    const routes = useMemo(()=>[
+        {   
+            icon:HiHome,
+            label:'Home',
+            active:pathname !=='/search',
+            href:'/'
+        },
+        {
+            icon:BiSearch,
+            label:'Search',
+            active:pathname !=='/search',
+            href:'/search'
+        },
+    ],[pathname])
+
     return(
-        <div>
-            {Children}
+        <div className="flex h-full">
+            <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+                <Box>
+                  <div className="flex flex-col gap-y-4 px-5 py-4 ">
+                    {routes.map((item)=>(
+                        <SidebarItem key={item.label} {...item} />
+                    ))}
+                    </div>
+                </Box>
+                <Box className="h-full overflow-y-auto">
+                    Song Libarary
+                </Box>
+            </div>
+            <main className='h-full flex-1 overflow-y-auto py-2'>
+                {children}
+            </main>
         </div>
     )
 }
